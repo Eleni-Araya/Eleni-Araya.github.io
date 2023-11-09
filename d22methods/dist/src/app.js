4af68972f82ec540272ec9f3d2b1548c405c6787
Eleni-Araya <elenib.araya@gmail.com>
2023-11-08 19:51:15 -0600

    calculator updated

--- d22methods/dist/src/app.js
+++
@@ -1,3 +0,0 @@
-//write any code you want to test here and run with npm run app
-console.log("i am runninng ");
-export {};
--- d22methods/src/calculator.ts
+++ d22methods/src/calculator.ts
@@ -1,6 +1,21 @@
-
-
 export const calculator = {
+    operand1:0,
+    operand2:0, 
+  
+    setValues:function(operand1:number,operand2:number){
+        this.operand1=operand1;//+prompt('a?',0)
+        this.operand2=operand2;
+    },
 
-};  // implement this
+    sum:function():number{
+        return this.operand1+this.operand2;
+    },
+
+    mul:function():number{
+        return this.operand1*this.operand2;
+    }
+
+} ;
+
+
 
